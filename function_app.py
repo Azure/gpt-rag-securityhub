1128892b71c8ff7c1c76fc01c566e4f34425d661
Mateo Perez Rivera <75545749+mateoperezrivera@users.noreply.github.com>
2024-12-18 09:59:08 -0300

    Added auditing endpoint

--- function_app.py
+++ function_app.py
@@ -4,17 +4,24 @@ import safety_checks.safety_checks as safety_checks
 import logging
 import asyncio
 import json
+import datetime
 from shared.util import get_secret
 from azure.core.credentials import AzureKeyCredential
 from azure.ai.contentsafety.aio import ContentSafetyClient
 from azure.identity.aio import DefaultAzureCredential
+from azure.cosmos.aio import CosmosClient
+
 
 
 app = func.FunctionApp(http_auth_level=func.AuthLevel.FUNCTION)
 CONTENT_SAFETY_ENDPOINT = os.environ.get("CONTENT_SAFETY_ENDPOINT")
 CONTENT_SAFETY_API_VERSION = os.environ.get("CONTENT_SAFETY_API_VERSION", "2024-02-15-preview")
 APIM_ENABLED=os.environ.get("APIM_ENABLED", "false").lower() == "true"
-
+AZURE_DB_URI=os.environ.get("AZURE_DB_URI")
+AZURE_DB_NAME=os.environ.get("AZURE_DB_NAME")
+AZURE_DB_ID=os.environ.get("AZURE_DB_ID")
+AZURE_DB_URI = f"https://{AZURE_DB_ID}.documents.azure.com:443/"
+logging.basicConfig(level=logging.INFO)
 @app.route(route="QuestionChecks")
 async def cf_question_checks(req: func.HttpRequest) -> func.HttpResponse:
     # Extract question, answer, and sources from the request
@@ -140,3 +147,43 @@ async def cf_answer_checks(req: func.HttpRequest) -> func.HttpResponse:
 
     # Return a JSON response
     return func.HttpResponse(json.dumps(response_data), status_code=200, mimetype="application/json")
+
+@app.route(route="Audit")
+async def audit(req: func.HttpRequest) -> func.HttpResponse:
+    try:
+        req_body = req.get_json()
+        question = req_body.get('question')
+        answer = req_body.get('answer')
+        sources = req_body.get('sources')
+        security_checks = req_body.get('security_checks')
+        conversation_id = req_body.get('conversation_id')
+    except ValueError:
+        return func.HttpResponse("Invalid request", status_code=400)
+
+    async with DefaultAzureCredential() as credential:       
+        async with CosmosClient(AZURE_DB_URI, credential=credential) as db_client:
+            db = db_client.get_database_client(database=AZURE_DB_NAME)
+            container = db.get_container_client('security_logs')
+            try:
+                conversation = await container.read_item(item=conversation_id, partition_key=conversation_id)
+                logging.info(f"[orchestrator] conversation {conversation_id} retrieved.")
+            except Exception as e:
+                logging.info(f"[orchestrator] customer sent an inexistent conversation_id, saving new conversation_id")        
+                conversation = await container.create_item(body={"id": conversation_id})
+            # get conversation data
+            conversation_data = conversation.get('conversation_data', 
+                                                {'start_date': datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"), 'interactions': []})
+            # Append the question and answer to the respective arrays
+            interaction = {
+                "time": datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
+                'question': question, 
+                'answer': answer, 
+                'sources': sources,
+                "security_checks": security_checks
+            }
+            conversation_data['interactions'].append(interaction)
+            conversation['conversation_data'] = conversation_data
+            # Update the item in the container
+            await container.replace_item(item=conversation, body=conversation)
+
+    return func.HttpResponse("Logging updated", status_code=200)
